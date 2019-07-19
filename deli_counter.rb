7645a7346d44fc05de3d0217b7a75b111278a96b
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-19 04:26:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli == []
-    if nextinline.length == 0
+    if katz_deli.length == 0
     puts "The line is currently empty."
   else 
     katz_deli.each.index(0) do |name, index|
