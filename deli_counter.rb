3a0fd4c7b3302c50af12f01706a32b73c500da38
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-19 04:14:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(nextinline)
+  if nextinline.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{nextinline[0]}."
+    nextinline.shift
+  end
+end
+
+def 
+end
